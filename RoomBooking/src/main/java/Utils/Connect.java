ec348ff6b2367bc1bec42dd9cb06dca2c40fad8e
Bichlienntbl2706 <bichlien.27062002@gmail.com>
2023-10-14 23:16:02 +0700

    BichLien update img

--- RoomBooking/src/main/java/Utils/Connect.java
+++ RoomBooking/src/main/java/Utils/Connect.java
@@ -16,7 +16,7 @@ import java.sql.Statement;
 public class Connect {
 
     private static String username = "sa";
-    private static String password = "123456";
+    private static String password = "123";
     private static String databaseName = "FPTBooking";
     private static String serverName = "localhost";
     private static int port = 1433;
